7744050d1b3b238f1ce3fb6b857d993f6f669d2f
Aadesh Agrawal <74392722+aadesh-2806@users.noreply.github.com>
2021-04-26 12:26:23 +0530

    Update ARRAY3.C

--- ARRAY3.C
+++ ARRAY3.C
@@ -1,3 +1,15 @@
+/*
+sum of array using while loop
+Enter number of elements: 5
+Enter value: 12
+Enter value: 23
+Enter value: 43
+Enter value: 45
+Enter value: 24
+147
+*/
+
+
 #include<stdio.h>
 #include<conio.h>
 int main()
